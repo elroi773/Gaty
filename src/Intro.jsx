0c5c39fd14d07ce123a29783cb45d300eec5a5f1
elroi773 <s2457@e-mirim.hs.kr>
2025-11-15 20:39:43 +0900

    기본 라우터 설정

---
+++ src/Intro.jsx
@@ -0,0 +1,10 @@
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+
+export default function Intro() {
+  return(
+    <div>
+        <h1>Intro Page 입니다</h1>
+    </div>
+  )
+}

593c31a1e98a789a4a5f3c5ff83fb95321cb4f2f
imdongdong0227 <barinlim77@gmail.com>
2025-11-22 20:51:52 +0900

    feature/join 프론트 작업

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
